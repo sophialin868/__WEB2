6cfff1fba7f2ef2016dbbcc1ed1bb634bba8948e
sophialin868 <stacy_lin868@yahoo.com.tw>
2022-12-24 15:08:41 +0800

    commit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template,request
-
 app = Flask(__name__)
-
 @app.route("/",methods=['GET','POST'])
-def index():
-        return render_template('index.html')
+def index():    
+    return render_template('index.html')
+
 @app.route('/name/<username>')
 def show_user(username):
-    return f"<h3>您的名字是<strong>{username}</strong></h3>"
+    return f"<h3>您的姓名是<strong>{username}</strong></h3>"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
