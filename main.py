c41130caf09bc25be3e3b35bb599b50cd67bef38
sophialin868 <stacy_lin868@yahoo.com.tw>
2022-12-24 14:13:51 +0800

    commi

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/",methods=['GET','POST'])
 def index():
-    
-    return render_template('index.html')
+        return render_template('index.html')
+@app.route('/name/<username>')
+def show_user(username):
+    return f"<h3>您的名字是<strong>{username}</strong></h3>"
